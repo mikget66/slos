85b8186cdf67fd19feb515a39f5d4176b034fb2c
Michael <mikelhefzy@gmail.com>
2022-10-04 10:58:12 +0200

    setup the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import Card from './component/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Card />s
+    </div>
+  );
+}
+
+export default App;
